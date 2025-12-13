9c2230864928eb03223285d4ba221b464ea18958
Błażej Drozd <drozdbazej@gmail.com>
2025-12-13 13:06:47 +0100

    Add function to extract pagination

--- src/search.rs
+++ src/search.rs
@@ -1,7 +1,8 @@
+use reqwest::Url;
 use scraper::{ElementRef, Html, Selector};
 use crate::error::ShindenError;
 use crate::models::{SearchAnimeItem, SearchPageResult, SearchRatings};
-use crate::utils::{extract_f64_after_colon, get_attr_from_selector, get_text_from_selector, parse_f64_from_comma_str, parse_u32_from_str};
+use crate::utils::{extract_f64_after_colon, get_attr_from_selector, get_text_from_selector, get_u32_param_from_url, parse_f64_from_comma_str, parse_u32_from_str};
 
 fn parse_anime_row(row: &ElementRef) -> Result<Option<SearchAnimeItem>, ShindenError> {
     let title_sel = Selector::parse(".desc-col h3 a")
@@ -93,4 +94,29 @@ fn extract_ratings(row: &ElementRef) -> Result<Option<SearchRatings>, ShindenErr
         music: extract_f64_after_colon(row, &rate_music_sel),
         characters: extract_f64_after_colon(row, &rate_chars_sel)
     }))
+}
+
+fn extract_pagination(doc: &Html) -> Result<(u32, u32), ShindenError> {
+    let pagination_sel = Selector::parse(".pagination")
+        .map_err(|e| ShindenError::HtmlError(e.to_string()))?;
+
+    let pagination_el = match doc.select(&pagination_sel).next() {
+        Some(el) => el,
+        None => return Ok((1, 1))
+    };
+
+    let current_page_sel = Selector::parse("li strong")
+        .map_err(|e| ShindenError::HtmlError(e.to_string()))?;
+    let last_page_sel = Selector::parse("li a[rel='last']")
+        .map_err(|e| ShindenError::HtmlError(e.to_string()))?;
+
+    let current_txt = get_text_from_selector(&pagination_el, &current_page_sel);
+    let current_page = parse_u32_from_str(&current_txt).unwrap_or(1);
+
+    let last_url_href = get_attr_from_selector(&pagination_el, &last_page_sel, "href");
+
+    let total_pages = get_u32_param_from_url(&last_url_href, "page")
+        .unwrap_or(current_page);
+
+    Ok((current_page, total_pages))
 }
